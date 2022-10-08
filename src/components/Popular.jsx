d3076218676b4ce99b0b0136eddcfb8f09a50d8a
Hanzhi <Hanzhi100@outlook.com>
2022-10-08 18:53:21 -0400

    style:basic styling for popular page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
   //save our retrieve data into a state
@@ -27,13 +28,35 @@ function Popular() {
     <div>
       {popular.map((recipe) => {
         return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
+          <Wrapper>
+            <h3>Popular Recipe</h3>
+            {popular.map((recipe) => {
+              return (
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              );
+            })}
+          </Wrapper>
         );
       })}
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0 rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+  }
+`;
 
 export default Popular;
