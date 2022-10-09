5da4345ca595750e460335ec568d0af58acf6485
Hanzhi <Hanzhi100@outlook.com>
2022-10-08 23:49:08 -0400

    style:create nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Pages from "./pages/Pages";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <Search />
       <Pages />
     </div>
   );
