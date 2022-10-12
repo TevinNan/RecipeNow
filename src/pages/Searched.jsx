00adf38bbdab27bef45bbdb1c750c1340e683dca
Hanzhi <Hanzhi100@outlook.com>
2022-10-11 23:15:38 -0400

    style:searchedPage styling

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function Searched() {
   const [searchedRecipes, setSearchedRecipes] = useState([]);
@@ -18,7 +19,36 @@ function Searched() {
     getSearched(params.search);
   }, [params.search]);
 
-  return <div>Searched</div>;
+  return (
+    <Grid>
+      {searchedRecipes.map((item) => {
+        return (
+          <Card key={item.id}>
+            <img src={item.image} alt="" />
+            <h4>{item.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 }
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 export default Searched;
