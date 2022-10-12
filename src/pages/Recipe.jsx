37f3fd3a95a8f5d3d8f73b28619ca60f7c29f90b
Hanzhi <Hanzhi100@outlook.com>
2022-10-11 23:31:20 -0400

    build:add RecipePage logic

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -5,7 +5,23 @@ import { useParams } from "react-router-dom";
 import React from "react";
 
 function Recipe() {
-  return <div>Recipe</div>;
+  let params = useParams();
+
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async () => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+    const detailData = await data.json();
+    setDetails(detailData);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, [params.name]);
+
+  return <div>{details.title}</div>;
 }
 
 export default Recipe;
