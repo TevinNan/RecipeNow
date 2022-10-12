66f0fac5a3350ae2067773478072df7db76c77fb
Hanzhi <Hanzhi100@outlook.com>
2022-10-11 23:19:09 -0400

    build:add RecipePage

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Home";
 import { Route, Routes } from "react-router-dom";
 import Searched from "./Searched";
+import Recipe from "./Recipe";
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home></Home>} />
       <Route path="/searched/:search" element={<Searched />} />
+      <Route path="/recipe/:name" element={<Recipe />} />
     </Routes>
   );
 }
