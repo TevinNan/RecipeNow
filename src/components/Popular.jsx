5ec796e8a720279c39ef59536e28910723d4df5f
igool999 <HYEO017@E.NTU.EDU.SG>
2022-10-15 23:32:44 -0400

    aesthetic changes to home page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -23,7 +23,7 @@ function Popular() {
       setPopular(JSON.parse(check));
     } else {
       const api = await fetch(
-        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+          `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
       );
       const data = await api.json();
       localStorage.setItem("popular", JSON.stringify(data.recipes));
@@ -36,37 +36,37 @@ function Popular() {
   //key = ... is to remove the unique key error, this is to ensure if data gets removed
   //from data base, the website can response accordingly.
   return (
-    <div>
-      <Wrapper>
-        <h3>Popular Recipe</h3>
+      <div>
+        <Wrapper>
+          <h3>Popular Recipe</h3>
 
-        <Splide
-          options={{
-            perPage: 4,
-            drag: "free",
-            gap: "3rem",
-          }}
-        >
-          {popular.map((recipe) => {
-            return (
-              <SplideSlide key={recipe.id}>
-                <Card>
-                  <Link to={"/recipe/" + recipe.id}>
-                    <p>{recipe.title}</p>
-                    <img
-                      src={recipe.image}
-                      width={300}
-                      height={250}
-                      alt={recipe.title}
-                    />
-                  </Link>
-                </Card>
-              </SplideSlide>
-            );
-          })}
-        </Splide>
-      </Wrapper>
-    </div>
+          <Splide
+              options={{
+                perPage: 4,
+                drag: "free",
+                gap: "3rem",
+              }}
+          >
+            {popular.map((recipe) => {
+              return (
+                  <SplideSlide key={recipe.id}>
+                    <Card>
+                      <Link to={"/recipe/" + recipe.id}>
+                        <p>{recipe.title}</p>
+                        <img
+                            src={recipe.image}
+                            width={300}
+                            height={250}
+                            alt={recipe.title}
+                        />
+                      </Link>
+                    </Card>
+                  </SplideSlide>
+              );
+            })}
+          </Splide>
+        </Wrapper>
+      </div>
   );
 }
 
@@ -78,10 +78,38 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
-
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 650;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5);
 `;
 
 export default Popular;
